34f9496d6cb1de71c8efeb1409c1543181554e0c
leungs420 <sunnyleungkc@hotmail.com>
2018-01-08 18:41:36 -0800

    Added sharedpreferences to the app so that the edittext dont get vanished

--- app/src/main/java/project/cs/t00531851/comp3160_activity1/MainActivity.java
+++ app/src/main/java/project/cs/t00531851/comp3160_activity1/MainActivity.java
@@ -1,6 +1,8 @@
 package project.cs.t00531851.comp3160_activity1;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,11 +11,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText);
 
         String result = "";
 
@@ -25,11 +28,21 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.result_view);
         textView.setText(result);
 
+
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String text = preferences.getString("original", "");
+        editText.setText(text);
+
         Button btn = (Button) findViewById(R.id.btn1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText editText = (EditText) findViewById(R.id.editText);
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("original", editText.getText().toString());
+                editor.apply();
+
                 String text = editText.getText().toString();
 
                 Intent send = new Intent(MainActivity.this, Main2Activity.class);
